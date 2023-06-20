bed76fe1c9ee394289f31da33b637940e3e140e1
Les <les.floresbae@gmail.com>
2023-06-19 20:46:06 -0600

    Se corrigio el código

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');/*SE CAMBIARON A CLASES (name y blog)*/
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {/*SE AGREGO ASYNC */
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; /*SE PUSIERON COMILLAS INVERSAS*/ 
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; /*SE AGREGO $ A n*/
 }
 
 displayUser('stolinski').catch(handleError);
